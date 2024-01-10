5560a7101712a3476214588d49c916caba58f007
taylor.martinez <taylor.swot.2018@gmail.com>
2024-01-09 20:33:14 -0600

    created models folder and users file and created users model

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+// const assignmentSchema = require('./Assignment');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: '/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,'
+    },
+    thoughts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Thought',
+    }],
+    friends: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      }],
+    });
+    userSchema.virtual('friendCount').get(function() {
+        return this.friends.length;
+      });
+
+const User = model('user', userSchema);
+
+module.exports = User;
